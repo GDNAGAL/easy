fe1e1da62bfbfdc8b60ffc06c1ef01de7c0c55d1
GDNAGAL <128776145+GDNAGAL@users.noreply.github.com>
2024-04-06 17:52:56 +0530

    update

--- custom/js/FillMarksSelectClass.js
+++ custom/js/FillMarksSelectClass.js
@@ -66,37 +66,46 @@ $( document ).ready(function() {
                  $.each(result.ClassRoom, function(i, item) {  
                     let bar; 
                     let MarkseetBtn ="";
+                    let AttenBtn;
+                    let ResultSheetBtn;
                     CompletedMarksAverage+=Number(item.CompletedPercent);
                     if(item.CompletedPercent == 0){
                         bar = `<div class="progress">
                                         <div class="progress-bar progress-bar-success" style="width: 0%"></div>
                                     </div>`
                         MarkseetBtn = `<button class="btn btn-sm btn-primary" disabled>View Marksheets</button>`;
+                        ResultSheetBtn = `<button class="btn btn-sm btn-info" disabled>View ResultSheet</button>`;
                     }else if(item.CompletedPercent == 100){
                         bar = `<div class="progress">
                                     <div class="progress-bar progress-bar-success" style="width: ${item.CompletedPercent}%">${item.CompletedPercent}%</div>
                                 </div>`
                         MarkseetBtn = `<a href="ViewMarksheets?ClassRoomID=${item.ClassRoomID}&SectionID=${item.SectionID}"><button class="btn btn-sm btn-primary">View Marksheets</button></a>`;
+                        ResultSheetBtn = `<a target="_blank" href="ResultSheet/ResultSheet?ClassRoomID=${item.ClassRoomID}&SectionID=${item.SectionID}"><button class="btn btn-sm btn-info">View ResultSheet</button></a>`;
+                        
                     }else{
                         bar = `<div class="progress progress-striped active">
                                     <div class="progress-bar progress-bar-danger" style="width: ${item.CompletedPercent}%">${item.CompletedPercent}%</div>
                                 </div>`
                         MarkseetBtn =  `<button class="btn btn-sm btn-primary" disabled>View Marksheets</button>`;
+                        ResultSheetBtn =  `<button class="btn btn-sm btn-info" disabled>View ResultSheet</button>`;
+                        
                     }
                     if(item.Students>0){
                         fillbtn =`<a href="MarksEntry?ClassRoomID=${item.ClassRoomID}&SectionID=${item.SectionID}&text=${item.ClassRoomName} ${item.SectionText}"><button class="btn btn-sm btn-success">Fill Marks</button></a>`; 
+                        AttenBtn = `<a href="AttendenceEntry?ClassRoomID=${item.ClassRoomID}&SectionID=${item.SectionID}"><button class="btn btn-sm btn-warning">Fill Attendence</button></a>`;
                     }else{
                         fillbtn = `<button title="No Student in Class." class="btn btn-sm btn-danger" disabled>Fill Marks</button>`;
+                        AttenBtn =  `<button class="btn btn-sm btn-warning" disabled>Fill Attendence</button>`;
                     }
                     var rawhtml = `<tr>
                     <td>${i+1}</td>
                     <td>${item.ClassRoomName} ${item.SectionText}</td>
                     <td class="text-center">${item.Students}</td>
-                    <td>
+                    <td style="width:35%">
                       ${bar}
                     </td>
-                    <td style="text-align:left">
-                    ${fillbtn} ${MarkseetBtn}</td></tr>`;       
+                    <td style="text-align:right;">
+                    ${fillbtn} ${MarkseetBtn} ${AttenBtn} ${ResultSheetBtn}</td></tr>`;       
                     $("#exambody").append(rawhtml)
                  })
                  $("#mfper").html((CompletedMarksAverage/ccount).toFixed(2)+" %")
